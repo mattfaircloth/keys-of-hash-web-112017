93a9494324a9fee4bfa67f91baea7d46d8573898
Matt Faircloth <mattcfaircloth@gmail.com>
2017-11-14 23:13:56 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,6 +1,6 @@
 
 class Hash
   def keys_of(*arguments)
-    map {|animal, country| arguments.include?(country) ? animal : nil }.collect
+    map {|animal, country| arguments.include?(country) ? animal : nil }.compact
   end
 end
