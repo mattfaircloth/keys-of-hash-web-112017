764409cd55956202e5af109a25dd7ee02dab3687
Matt Faircloth <mattcfaircloth@gmail.com>
2017-11-14 22:58:48 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,10 +2,19 @@ class Hash
   def keys_of(*arguments)
     final = []
     arguments.each do |animal, country|
-      if arguments == animal[country]
-        final << animal
+       if arguments == country
+         final << animal
+       else
+         nil
       end
     end
     return final
   end
 end
+
+
+# class Hash
+#   def keys_of(*args)
+#     map {|key, value| args.include?(value) ? key : nil }.compact
+#   end
+# end
