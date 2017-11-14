e4196a9cd77eca63478b1acde55b4e520252ebb6
Matt Faircloth <mattcfaircloth@gmail.com>
2017-11-14 22:42:45 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,8 +1,8 @@
 class Hash
   def keys_of(*arguments)
     final = []
-    arguments.each do |item|
-      final << item
+    arguments.each do |key, value|
+      final << value
     end
     return final
   end
